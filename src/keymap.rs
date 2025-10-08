a4505b2e28e4e852758ced1dc33015fb79217d5e
Alignof <Alignof@outlook.com>
2025-10-09 02:55:48 +0900

    [update] update keymap data

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,23 +1,19 @@
 use rmk::action::KeyAction;
-use rmk::{a, k, layer, mo};
+use rmk::{k, layer, mo};
 pub(crate) const COL: usize = 3;
-pub(crate) const ROW: usize = 4;
+pub(crate) const ROW: usize = 2;
 pub(crate) const NUM_LAYER: usize = 2;
 
 #[rustfmt::skip]
 pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
     [
         layer!([
-            [k!(Kp7), k!(Kp8), k!(Kp9)],
-            [k!(Kp4), k!(Kp5), k!(Kp6)],
-            [k!(Kp1), k!(Kp2), k!(Kp3)],
-            [mo!(1), a!(No), k!(Kp0)]
+            [k!(A), k!(B), k!(Backspace)],
+            [k!(C), k!(D), mo!(1)]
         ]),
         layer!([
-            [k!(Kp7), k!(Kp8), k!(Kp9)],
-            [k!(Kp4), k!(Kp5), k!(Kp6)],
-            [k!(Kp1), k!(Kp2), k!(Kp3)],
-            [mo!(1), a!(No), k!(Kp0)]
+            [k!(E), k!(F), k!(Enter)],
+            [k!(Kc4), k!(Kc5), k!(Kc6)]
         ]),
     ]
 }
