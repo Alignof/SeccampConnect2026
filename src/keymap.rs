026362f2ea1f9825b6889dbef3cd75babc19836c
Alignof <Alignof@outlook.com>
2025-12-20 14:52:38 +0900

    [add] support for rotary encoder

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,8 +1,11 @@
-use rmk::action::KeyAction;
+use rmk::encoder;
+use rmk::types::action::{EncoderAction, KeyAction};
 use rmk::{k, layer, mo};
+
 pub(crate) const COL: usize = 3;
 pub(crate) const ROW: usize = 2;
 pub(crate) const NUM_LAYER: usize = 2;
+pub(crate) const NUM_ENCODER: usize = 1;
 
 #[rustfmt::skip]
 pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
@@ -15,5 +18,19 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
             [k!(E), k!(F), k!(Enter)],
             [k!(Kc4), k!(Kc5), k!(Kc6)]
         ]),
+    ]
+}
+
+#[rustfmt::skip]
+pub const fn get_default_encoder_map() -> [[EncoderAction; NUM_ENCODER]; NUM_LAYER] {
+    [
+        [
+            // Encoder 0: (Clockwise, Counter-Clockwise)
+            encoder!(k!(MouseWheelUp), k!(MouseWheelDown)),
+        ],
+        [
+            // Encoder 0: (Clockwise, Counter-Clockwise)
+            encoder!(k!(MouseWheelUp), k!(MouseWheelDown)),
+        ],
     ]
 }
--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,26 @@ use bt_hci::controller::ExternalController;
 use embassy_executor::Spawner;
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull};
-use esp_hal::otg_fs::asynch::{Config, Driver};
 use esp_hal::otg_fs::Usb;
+use esp_hal::otg_fs::asynch::{Config, Driver};
+use esp_hal::rng::TrngSource;
 use esp_hal::timer::timg::TimerGroup;
+use esp_radio::Controller;
+use esp_radio::ble::controller::BleConnector;
 use esp_storage::FlashStorage;
-use esp_wifi::ble::controller::BleConnector;
-use rmk::ble::trouble::build_ble_stack;
+use rmk::ble::build_ble_stack;
 use rmk::channel::EVENT_CHANNEL;
-use rmk::config::{BehaviorConfig, ControllerConfig, RmkConfig, StorageConfig, VialConfig};
+use rmk::config::{BehaviorConfig, PositionalConfig, RmkConfig, StorageConfig, VialConfig};
 use rmk::debounce::default_debouncer::DefaultDebouncer;
 use rmk::futures::future::join3;
 use rmk::input_device::Runnable;
+use rmk::input_device::rotary_encoder::DefaultPhase;
+use rmk::input_device::rotary_encoder::RotaryEncoder;
 use rmk::keyboard::Keyboard;
-use rmk::light::LightController;
 use rmk::matrix::Matrix;
 use rmk::storage::async_flash_wrapper;
-use rmk::{initialize_keymap_and_storage, run_devices, run_rmk, HostResources};
+use rmk::{HostResources, initialize_encoder_keymap_and_storage, run_devices, run_rmk};
+use static_cell::StaticCell;
 use {esp_alloc as _, esp_backtrace as _};
 
 use crate::keymap::*;
@@ -35,19 +39,26 @@ use crate::vial::{VIAL_KEYBOARD_DEF, VIAL_KEYBOARD_ID};
 
 ::esp_bootloader_esp_idf::esp_app_desc!();
 
-#[esp_hal_embassy::main]
+#[esp_rtos::main]
 async fn main(_s: Spawner) {
     // Initialize the peripherals and bluetooth controller
     esp_println::logger::init_logger_from_env();
     let peripherals = esp_hal::init(esp_hal::Config::default().with_cpu_clock(CpuClock::max()));
     esp_alloc::heap_allocator!(size: 72 * 1024);
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let mut rng = esp_hal::rng::Trng::new(peripherals.RNG, peripherals.ADC1);
-    let init = esp_wifi::init(timg0.timer0, rng.rng.clone()).unwrap();
-    let systimer = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER);
-    esp_hal_embassy::init(systimer.alarm0);
+    #[cfg(target_arch = "riscv32")]
+    let software_interrupt = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(
+        timg0.timer0,
+        #[cfg(target_arch = "riscv32")]
+        software_interrupt.software_interrupt0,
+    );
+    let _trng_source = TrngSource::new(peripherals.RNG, peripherals.ADC1);
+    let mut rng = esp_hal::rng::Trng::try_new().unwrap();
+    static RADIO: StaticCell<Controller<'static>> = StaticCell::new();
+    let radio = RADIO.init(esp_radio::init().unwrap());
     let bluetooth = peripherals.BT;
-    let connector = BleConnector::new(&init, bluetooth);
+    let connector = BleConnector::new(radio, bluetooth, Default::default()).unwrap();
     let controller: ExternalController<_, 20> = ExternalController::new(connector);
     let central_addr = [0x18, 0xe2, 0x21, 0x80, 0xc0, 0xc7];
     let mut host_resources = HostResources::new();
@@ -61,14 +72,14 @@ async fn main(_s: Spawner) {
     let usb_driver = Driver::new(usb, unsafe { &mut *addr_of_mut!(EP_MEMORY) }, config);
 
     // Initialize the flash
-    let flash = FlashStorage::new();
+    let flash = FlashStorage::new(peripherals.FLASH);
     let flash = async_flash_wrapper(flash);
 
     // Initialize the IO pins
-    let (input_pins, output_pins) = config_matrix_pins_esp!(peripherals: peripherals, input: [GPIO8, GPIO4], output: [GPIO3, GPIO5, GPIO6]);
+    let (row_pins, col_pins) = config_matrix_pins_esp!(peripherals: peripherals, input: [GPIO8, GPIO4], output: [GPIO3, GPIO5, GPIO6]);
 
     // RMK config
-    let vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF);
+    let vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF, &[(0, 0), (1, 1)]);
     let storage_config = StorageConfig {
         start_addr: 0x3f0000,
         num_sectors: 16,
@@ -83,34 +94,36 @@ async fn main(_s: Spawner) {
     // Initialze keyboard stuffs
     // Initialize the storage and keymap
     let mut default_keymap = keymap::get_default_keymap();
-    let behavior_config = BehaviorConfig::default();
-    let (keymap, mut storage) =
-        initialize_keymap_and_storage(&mut default_keymap, flash, &storage_config, behavior_config)
-            .await;
+    let mut behavior_config = BehaviorConfig::default();
+    let mut encoder_map = keymap::get_default_encoder_map();
+    let mut per_key_config = PositionalConfig::default();
+    let (keymap, mut storage) = initialize_encoder_keymap_and_storage(
+        &mut default_keymap,
+        &mut encoder_map,
+        flash,
+        &storage_config,
+        &mut behavior_config,
+        &mut per_key_config,
+    )
+    .await;
 
     // Initialize the matrix and keyboard
-    let debouncer = DefaultDebouncer::<ROW, COL>::new();
-    let mut matrix = Matrix::<_, _, _, ROW, COL>::new(input_pins, output_pins, debouncer);
+    let debouncer = DefaultDebouncer::new();
+    let mut matrix = Matrix::<_, _, _, ROW, COL, true>::new(row_pins, col_pins, debouncer);
     // let mut matrix = rmk::matrix::TestMatrix::<ROW, COL>::new();
     let mut keyboard = Keyboard::new(&keymap); // Initialize the light controller
 
-    // Initialize the light controller
-    let mut light_controller: LightController<Output> =
-        LightController::new(ControllerConfig::default().light_config);
+    // Initialize rotary encoder
+    let pin_a = Input::new(peripherals.GPIO43, InputConfig::default());
+    let pin_b = Input::new(peripherals.GPIO44, InputConfig::default());
+    let mut encoder = RotaryEncoder::with_phase(pin_a, pin_b, DefaultPhase, 0);
 
     join3(
         run_devices! (
-            (matrix) => EVENT_CHANNEL,
+            (matrix, encoder) => EVENT_CHANNEL,
         ),
         keyboard.run(), // Keyboard is special
-        run_rmk(
-            &keymap,
-            usb_driver,
-            &stack,
-            &mut storage,
-            &mut light_controller,
-            rmk_config,
-        ),
+        run_rmk(&keymap, usb_driver, &stack, &mut storage, rmk_config),
     )
     .await;
 }
